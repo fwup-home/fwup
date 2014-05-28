68eb0a5259a7481f0a7f46ddf892769eb7adc5d5
Frank Hunleth <fhunleth@troodon-software.com>
2014-05-28 16:56:00 -0400

    More copyrights

--- fwup_metadata.c
+++ fwup_metadata.c
@@ -1,8 +1,23 @@
+/*
+ * Copyright 2014 LKC Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "fwup_metadata.h"
 #include <confuse.h>
 #include <err.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "cfgfile.h"
 #include "util.h"
