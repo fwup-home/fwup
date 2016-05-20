4895c5b1ec3d889d9c060c47a54ab0359a655bd0
Frank Hunleth <fhunleth@troodon-software.com>
2016-05-20 16:17:01 -0400

    Update and break up static build script

--- scripts/build_pkg.sh
+++ scripts/build_pkg.sh
@@ -10,10 +10,9 @@
 #
 set -e
 
-BASE_DIR=$(pwd)
-BUILD_DIR=$BASE_DIR/pkg_build
-DOWNLOAD_DIR=$BASE_DIR/dl
-INSTALL_DIR=$BUILD_DIR/usr
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+BUILD_DIR=$BASE_DIR/build
+DEPS_INSTALL_DIR=$DEPS_DIR/usr
 
 FWUP_INSTALL_DIR=$BUILD_DIR/fwup-installed/usr
 
@@ -30,57 +29,19 @@ if [[ -e $BASE_DIR/Makefile ]]; then
     make distclean
 fi
 
+# Build the dependencies
+$BASE_DIR/scripts/download_deps.sh
+
 # Initialize some directories
-mkdir -p $DOWNLOAD_DIR
 mkdir -p $BUILD_DIR
-mkdir -p $INSTALL_DIR
 mkdir -p $FWUP_INSTALL_DIR
 
 pushd $BUILD_DIR
 
-if [[ ! -e $DOWNLOAD_DIR/.downloaded ]]; then
-    pushd $DOWNLOAD_DIR
-    wget https://github.com/martinh/libconfuse/releases/download/v2.8/confuse-2.8.tar.xz
-    wget http://libarchive.org/downloads/libarchive-3.1.2.tar.gz
-    wget https://download.libsodium.org/libsodium/releases/libsodium-1.0.8.tar.gz
-    touch $DOWNLOAD_DIR/.downloaded
-    popd
-fi
-
-if [[ ! -e $INSTALL_DIR/lib/libconfuse.a ]]; then
-    rm -fr $BUILD_DIR/confuse-2.8
-    tar xf $DOWNLOAD_DIR/confuse-2.8.tar.xz
-    pushd confuse-2.8
-    ./configure --prefix=$INSTALL_DIR --disable-examples --enable-shared=no
-    make $MAKE_FLAGS
-    make install
-    popd
-fi
-
-if [[ ! -e $INSTALL_DIR/lib/libarchive.a ]]; then
-    rm -fr libarchive-3.1.2
-    tar xf $DOWNLOAD_DIR/libarchive-3.1.2.tar.gz
-    pushd libarchive-3.1.2
-    ./configure --prefix=$INSTALL_DIR --without-xml2 --without-openssl --without-nettle --without-expat --without-lzo2 --without-lzma --without-bz2lib --enable-shared=no
-    make $MAKE_FLAGS
-    make install
-    popd
-fi
-
-if [[ ! -e $INSTALL_DIR/lib/libsodium.a ]]; then
-    rm -fr libsodium-3.1.2
-    tar xf $DOWNLOAD_DIR/libsodium-1.0.8.tar.gz
-    pushd libsodium-1.0.8
-    ./configure --prefix=$INSTALL_DIR --enable-shared=no
-    make $MAKE_FLAGS
-    make install
-    popd
-fi
-
 # Build fwup (symlink now, since out-of-tree fwup build is broke)
 ln -sf $BASE_DIR $BUILD_DIR/fwup
 pushd fwup
-LDFLAGS=-L$INSTALL_DIR/lib CPPFLAGS=-I$INSTALL_DIR/include ./configure --prefix=$FWUP_INSTALL_DIR --enable-shared=no
+LDFLAGS=-L$DEPS_INSTALL_DIR/lib CPPFLAGS=-I$DEPS_INSTALL_DIR/include ./configure --prefix=$FWUP_INSTALL_DIR --enable-shared=no
 make clean
 make $MAKE_FLAGS
 make check
---
+++ scripts/download_deps.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+#
+# Download and build dependencies as static libs
+#
+set -e
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+DEPS_DIR=$BASE_DIR/deps
+DOWNLOAD_DIR=$DEPS_DIR/dl
+DEPS_INSTALL_DIR=$DEPS_DIR/usr
+
+LIBARCHIVE_VERSION=3.2.0
+LIBSODIUM_VERSION=1.0.10
+CONFUSE_VERSION=3.0
+
+MAKE_FLAGS=-j8
+
+# Initialize some directories
+mkdir -p $DOWNLOAD_DIR
+mkdir -p $DEPS_INSTALL_DIR
+
+pushd $DEPS_DIR
+
+if [[ ! -e $DOWNLOAD_DIR/.downloaded ]]; then
+    pushd $DOWNLOAD_DIR
+    wget https://github.com/martinh/libconfuse/releases/download/v$CONFUSE_VERSION/confuse-$CONFUSE_VERSION.tar.xz
+    wget http://libarchive.org/downloads/libarchive-$LIBARCHIVE_VERSION.tar.gz
+    wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VERSION.tar.gz
+    touch $DOWNLOAD_DIR/.downloaded
+    popd
+fi
+
+if [[ ! -e $DEPS_INSTALL_DIR/lib/libconfuse.a ]]; then
+    rm -fr $DEPS_DIR/confuse-*
+    tar xf $DOWNLOAD_DIR/confuse-$CONFUSE_VERSION.tar.xz
+    pushd confuse-$CONFUSE_VERSION
+    ./configure --prefix=$DEPS_INSTALL_DIR --disable-examples --enable-shared=no
+    make $MAKE_FLAGS
+    make install
+    popd
+fi
+
+if [[ ! -e $DEPS_INSTALL_DIR/lib/libarchive.a ]]; then
+    rm -fr libarchive-*
+    tar xf $DOWNLOAD_DIR/libarchive-$LIBARCHIVE_VERSION.tar.gz
+    pushd libarchive-$LIBARCHIVE_VERSION
+    ./configure --prefix=$DEPS_INSTALL_DIR --without-xml2 --without-openssl --without-nettle --without-expat --without-lzo2 --without-lzma --without-bz2lib --enable-shared=no
+    make $MAKE_FLAGS
+    make install
+    popd
+fi
+
+if [[ ! -e $DEPS_INSTALL_DIR/lib/libsodium.a ]]; then
+    rm -fr libsodium-*
+    tar xf $DOWNLOAD_DIR/libsodium-$LIBSODIUM_VERSION.tar.gz
+    pushd libsodium-$LIBSODIUM_VERSION
+    ./configure --prefix=$DEPS_INSTALL_DIR --enable-shared=no
+    make $MAKE_FLAGS
+    make install
+    popd
+fi
+
+# Return to the base directory
+popd
+
+echo Dependencies built successfully!
+echo
+echo To compile fwup statically with these libraries, run:
+echo
+echo ./autogen.sh    # if you're compiling from source
+echo LDFLAGS=-L$DEPS_INSTALL_DIR/lib CPPFLAGS=-I$DEPS_INSTALL_DIR/include ./configure --enable-shared=no
+echo make
+echo make check
+echo make install
