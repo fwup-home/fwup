ff5b6de9756b99dd2d0961ba16dd1739741d2f63
Frank Hunleth <fhunleth@troodon-software.com>
2016-06-11 14:20:23 -0400

    Add Windows support to download_deps.sh

--- scripts/download_deps.sh
+++ scripts/download_deps.sh
@@ -17,6 +17,13 @@ CONFUSE_VERSION=3.0
 
 MAKE_FLAGS=-j8
 
+UNAME_S=$(uname -s)
+if [[ $UNAME_S = "CYGWIN_NT-10.0" ]]; then
+    # Cygwin is required to buid, but we want fwup to run with only
+    # the mingw-w64 runtime library.
+    CONFIGURE_ARGS=--host=x86_64-w64-mingw32
+fi
+
 # Initialize some directories
 mkdir -p $DOWNLOAD_DIR
 mkdir -p $DEPS_INSTALL_DIR
@@ -45,7 +52,7 @@ if [[ ! -e $DEPS_INSTALL_DIR/lib/libconfuse.a ]]; then
     rm -fr $DEPS_DIR/confuse-*
     tar xf $DOWNLOAD_DIR/confuse-$CONFUSE_VERSION.tar.xz
     pushd confuse-$CONFUSE_VERSION
-    ./configure --prefix=$DEPS_INSTALL_DIR --disable-examples --enable-shared=no
+    ./configure $CONFIGURE_ARGS --prefix=$DEPS_INSTALL_DIR --disable-examples --enable-shared=no
     make $MAKE_FLAGS
     make install
     popd
@@ -55,7 +62,7 @@ if [[ ! -e $DEPS_INSTALL_DIR/lib/libarchive.a ]]; then
     rm -fr libarchive-*
     tar xf $DOWNLOAD_DIR/libarchive-$LIBARCHIVE_VERSION.tar.gz
     pushd libarchive-$LIBARCHIVE_VERSION
-    ./configure --prefix=$DEPS_INSTALL_DIR --without-xml2 --without-openssl --without-nettle --without-expat --without-lzo2 --without-lzma --without-bz2lib --without-iconv --enable-shared=no
+    ./configure $CONFIGURE_ARGS --prefix=$DEPS_INSTALL_DIR --without-xml2 --without-openssl --without-nettle --without-expat --without-lzo2 --without-lzma --without-bz2lib --without-iconv --enable-shared=no
     make $MAKE_FLAGS
     make install
     popd
@@ -65,7 +72,7 @@ if [[ ! -e $DEPS_INSTALL_DIR/lib/libsodium.a ]]; then
     rm -fr libsodium-*
     tar xf $DOWNLOAD_DIR/libsodium-$LIBSODIUM_VERSION.tar.gz
     pushd libsodium-$LIBSODIUM_VERSION
-    ./configure --prefix=$DEPS_INSTALL_DIR --enable-shared=no
+    ./configure $CONFIGURE_ARGS --prefix=$DEPS_INSTALL_DIR --enable-shared=no
     make $MAKE_FLAGS
     make install
     popd
@@ -79,7 +86,7 @@ echo
 echo To compile fwup statically with these libraries, run:
 echo
 echo ./autogen.sh    # if you're compiling from source
-echo LDFLAGS=-L$DEPS_INSTALL_DIR/lib CPPFLAGS=-I$DEPS_INSTALL_DIR/include ./configure --enable-shared=no
+echo LDFLAGS=-L$DEPS_INSTALL_DIR/lib CPPFLAGS=-I$DEPS_INSTALL_DIR/include ./configure $CONFIGURE_ARGS --enable-shared=no
 echo make
 echo make check
 echo make install
