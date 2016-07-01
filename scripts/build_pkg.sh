70a3ba0b719a3a9cbb4408a5cac4939d78531ea9
Greg Mefford <greg@gregmefford.com>
2016-07-01 11:59:26 -0400

    Build packages for both Linux and Windows

--- scripts/build_pkg.sh
+++ scripts/build_pkg.sh
@@ -9,16 +9,20 @@
 # This script creates a static build of fwup to avoid dependency issues
 # with libconfuse and libsodium on various Linux distributions. The result
 # is a self-contained .deb and .rpm that should work on any Linux (assuming
-# it's running on the same processor architecture)
+# it's running on the same processor architecture) and an .exe for Windows.
 #
 set -e
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 BUILD_DIR=$BASE_DIR/build
 DEPS_DIR=$BASE_DIR/deps
-DEPS_INSTALL_DIR=$DEPS_DIR/usr
+DEPS_INSTALL_DIR_NIX=$DEPS_DIR/nix/usr
+DEPS_INSTALL_DIR_WIN=$DEPS_DIR/win/usr
 
-FWUP_INSTALL_DIR=$BUILD_DIR/fwup-installed/usr
+FWUP_INSTALL_DIR_NIX=$BUILD_DIR/fwup-installed/nix/usr
+FWUP_INSTALL_DIR_WIN=$BUILD_DIR/fwup-installed/win/usr
+
+CONFIGURE_ARGS_WIN=--host=x86_64-w64-mingw32
 
 MAKE_FLAGS=-j8
 
@@ -42,15 +46,26 @@ $BASE_DIR/scripts/download_deps.sh
 
 # Initialize some directories
 mkdir -p $BUILD_DIR
-mkdir -p $FWUP_INSTALL_DIR
+mkdir -p $FWUP_INSTALL_DIR_NIX
+mkdir -p $FWUP_INSTALL_DIR_WIN
 
 pushd $BUILD_DIR
 
 # Build fwup (symlink now, since out-of-tree fwup build is broke)
 ln -sf $BASE_DIR $BUILD_DIR/fwup
 pushd fwup
-LDFLAGS="-L$DEPS_INSTALL_DIR/lib $EXTRA_LDFLAGS" CPPFLAGS=-I$DEPS_INSTALL_DIR/include ./configure --prefix=$FWUP_INSTALL_DIR --enable-shared=no || cat config.log
-cat config.log
+
+# Build for Windows first
+LDFLAGS="-L$DEPS_INSTALL_DIR_WIN/lib $EXTRA_LDFLAGS" CPPFLAGS=-I$DEPS_INSTALL_DIR_WIN/include ./configure $CONFIGURE_ARGS_WIN --prefix=$FWUP_INSTALL_DIR_WIN --enable-shared=no || cat config.log
+# cat config.log
+make clean
+make $MAKE_FLAGS
+make install-strip
+make dist
+
+# Build for Linux last so we can run the tests and sanity checks below
+LDFLAGS="-L$DEPS_INSTALL_DIR_NIX/lib $EXTRA_LDFLAGS" CPPFLAGS=-I$DEPS_INSTALL_DIR_NIX/include ./configure --prefix=$FWUP_INSTALL_DIR_NIX --enable-shared=no || cat config.log
+# cat config.log
 make clean
 make $MAKE_FLAGS
 
@@ -84,12 +99,13 @@ popd
 
 # Package fwup
 if [[ "$SKIP_PACKAGE" != "true" ]]; then
-    FWUP_VERSION=$($FWUP_INSTALL_DIR/bin/fwup --version)
+    FWUP_VERSION=$($FWUP_INSTALL_DIR_NIX/bin/fwup --version)
     rm -f fwup_*.deb fwup-*.rpm
-    fpm -s dir -t deb -v $FWUP_VERSION -n fwup -C $FWUP_INSTALL_DIR/..
-    fpm -s dir -t rpm -v $FWUP_VERSION -n fwup -C $FWUP_INSTALL_DIR/..
+    fpm -s dir -t deb -v $FWUP_VERSION -n fwup -C $FWUP_INSTALL_DIR_NIX/..
+    fpm -s dir -t rpm -v $FWUP_VERSION -n fwup -C $FWUP_INSTALL_DIR_NIX/..
 else
     echo "Static build was successful, but skipped creating packages."
-    echo "The fwup installation is in $FWUP_INSTALL_DIR."
+    echo "The fwup installation is in $FWUP_INSTALL_DIR_NIX for Linux"
+    echo "and $FWUP_INSTALL_DIR_WIN for Windows."
 fi
 
--- scripts/download_deps.sh
+++ scripts/download_deps.sh
@@ -13,20 +13,17 @@ CONFUSE_VERSION=3.0
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 DEPS_DIR=$BASE_DIR/deps
 DOWNLOAD_DIR=$DEPS_DIR/dl
-DEPS_INSTALL_DIR=$DEPS_DIR/usr
+DEPS_INSTALL_DIR_NIX=$DEPS_DIR/nix/usr
+DEPS_INSTALL_DIR_WIN=$DEPS_DIR/win/usr
 
 MAKE_FLAGS=-j8
 
-UNAME_S=$(uname -s)
-if [[ $UNAME_S = "CYGWIN_NT-10.0" ]]; then
-    # Cygwin is required to buid, but we want fwup to run with only
-    # the mingw-w64 runtime library.
-    CONFIGURE_ARGS=--host=x86_64-w64-mingw32
-fi
+CONFIGURE_ARGS_WIN=--host=x86_64-w64-mingw32
 
 # Initialize some directories
 mkdir -p $DOWNLOAD_DIR
-mkdir -p $DEPS_INSTALL_DIR
+mkdir -p $DEPS_INSTALL_DIR_NIX
+mkdir -p $DEPS_INSTALL_DIR_WIN
 
 pushd $DEPS_DIR
 
@@ -37,42 +34,85 @@ pushd $DOWNLOAD_DIR
 [[ -e libsodium-$LIBSODIUM_VERSION.tar.gz ]] || wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VERSION.tar.gz
 popd
 
-if [[ ! -e $DEPS_INSTALL_DIR/lib/libz.a ]]; then
+# Build zlib for Linux and Windows
+if [[ ! -e $DEPS_INSTALL_DIR_NIX/lib/libz.a ]]; then
     rm -fr $DEPS_DIR/zlib-*
     tar xf $DOWNLOAD_DIR/zlib-$ZLIB_VERSION.tar.xz
     pushd zlib-$ZLIB_VERSION
-    ./configure $CONFIGURE_ARGS --prefix=$DEPS_INSTALL_DIR --static
+    ./configure --prefix=$DEPS_INSTALL_DIR_NIX --static
     make $MAKE_FLAGS
     make install
     popd
 fi
 
+if [[ ! -e $DEPS_INSTALL_DIR_WIN/lib/libz.a ]]; then
+    rm -fr $DEPS_DIR/zlib-*
+    tar xf $DOWNLOAD_DIR/zlib-$ZLIB_VERSION.tar.xz
+    pushd zlib-$ZLIB_VERSION
+    CC=x86_64-w64-mingw32-gcc ./configure --prefix=$DEPS_INSTALL_DIR_WIN --static
+    make $MAKE_FLAGS
+    make install
+    popd
+fi
 
-if [[ ! -e $DEPS_INSTALL_DIR/lib/libconfuse.a ]]; then
+# Build libconfuse for Linux and Windows
+if [[ ! -e $DEPS_INSTALL_DIR_NIX/lib/libconfuse.a ]]; then
     rm -fr $DEPS_DIR/confuse-*
     tar xf $DOWNLOAD_DIR/confuse-$CONFUSE_VERSION.tar.xz
     pushd confuse-$CONFUSE_VERSION
-    ./configure $CONFIGURE_ARGS --prefix=$DEPS_INSTALL_DIR --disable-examples --enable-shared=no
+    ./configure --prefix=$DEPS_INSTALL_DIR_NIX --disable-examples --enable-shared=no
     make $MAKE_FLAGS
     make install
     popd
 fi
 
-if [[ ! -e $DEPS_INSTALL_DIR/lib/libarchive.a ]]; then
+if [[ ! -e $DEPS_INSTALL_DIR_WIN/lib/libconfuse.a ]]; then
+    rm -fr $DEPS_DIR/confuse-*
+    tar xf $DOWNLOAD_DIR/confuse-$CONFUSE_VERSION.tar.xz
+    pushd confuse-$CONFUSE_VERSION
+    ./configure $CONFIGURE_ARGS_WIN --prefix=$DEPS_INSTALL_DIR_WIN --disable-examples --enable-shared=no
+    make $MAKE_FLAGS
+    make install
+    popd
+fi
+
+# Build libarchive for Linux and Windows
+if [[ ! -e $DEPS_INSTALL_DIR_NIX/lib/libarchive.a ]]; then
     rm -fr libarchive-*
     tar xf $DOWNLOAD_DIR/libarchive-$LIBARCHIVE_VERSION.tar.gz
     pushd libarchive-$LIBARCHIVE_VERSION
-    ./configure $CONFIGURE_ARGS --prefix=$DEPS_INSTALL_DIR --without-xml2 --without-openssl --without-nettle --without-expat --without-lzo2 --without-lzma --without-bz2lib --without-iconv --enable-shared=no
+    ./configure --prefix=$DEPS_INSTALL_DIR_NIX --without-xml2 --without-openssl --without-nettle --without-expat --without-lzo2 --without-lzma --without-bz2lib --without-iconv --enable-shared=no
     make $MAKE_FLAGS
     make install
     popd
 fi
 
-if [[ ! -e $DEPS_INSTALL_DIR/lib/libsodium.a ]]; then
+if [[ ! -e $DEPS_INSTALL_DIR_WIN/lib/libarchive.a ]]; then
+    rm -fr libarchive-*
+    tar xf $DOWNLOAD_DIR/libarchive-$LIBARCHIVE_VERSION.tar.gz
+    pushd libarchive-$LIBARCHIVE_VERSION
+    ./configure $CONFIGURE_ARGS_WIN --prefix=$DEPS_INSTALL_DIR_WIN --without-xml2 --without-openssl --without-nettle --without-expat --without-lzo2 --without-lzma --without-bz2lib --without-iconv --enable-shared=no
+    make $MAKE_FLAGS
+    make install
+    popd
+fi
+
+# Build libsodium for Linux and Windows
+if [[ ! -e $DEPS_INSTALL_DIR_NIX/lib/libsodium.a ]]; then
     rm -fr libsodium-*
     tar xf $DOWNLOAD_DIR/libsodium-$LIBSODIUM_VERSION.tar.gz
     pushd libsodium-$LIBSODIUM_VERSION
-    ./configure $CONFIGURE_ARGS --prefix=$DEPS_INSTALL_DIR --enable-shared=no
+    ./configure --prefix=$DEPS_INSTALL_DIR_NIX --enable-shared=no
+    make $MAKE_FLAGS
+    make install
+    popd
+fi
+
+if [[ ! -e $DEPS_INSTALL_DIR_WIN/lib/libsodium.a ]]; then
+    rm -fr libsodium-*
+    tar xf $DOWNLOAD_DIR/libsodium-$LIBSODIUM_VERSION.tar.gz
+    pushd libsodium-$LIBSODIUM_VERSION
+    ./configure $CONFIGURE_ARGS_WIN --prefix=$DEPS_INSTALL_DIR_WIN --enable-shared=no
     make $MAKE_FLAGS
     make install
     popd
@@ -85,8 +125,11 @@ echo Dependencies built successfully!
 echo
 echo To compile fwup statically with these libraries, run:
 echo
-echo ./autogen.sh    # if you're compiling from source
-echo LDFLAGS=-L$DEPS_INSTALL_DIR/lib CPPFLAGS=-I$DEPS_INSTALL_DIR/include ./configure $CONFIGURE_ARGS --enable-shared=no
+echo "./autogen.sh # if you're compiling from source"
+echo "# For Linux:"
+echo LDFLAGS=-L$DEPS_INSTALL_DIR_NIX/lib CPPFLAGS=-I$DEPS_INSTALL_DIR_NIX/include ./configure --enable-shared=no
+echo "# For Windows"
+echo LDFLAGS=-L$DEPS_INSTALL_DIR_WIN/lib CPPFLAGS=-I$DEPS_INSTALL_DIR_WIN/include ./configure $CONFIGURE_ARGS_WIN --enable-shared=no
 echo make
 echo make check
 echo make install
