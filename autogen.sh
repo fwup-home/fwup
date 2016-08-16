71a3fad5699f0e37d8a26df4887a9a95616cf7de
Frank Hunleth <fhunleth@troodon-software.com>
2016-08-15 21:30:52 -0400

    Fix ./configure error on Windows 10 bash mode

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 autoreconf --install
+
+if [[ -d "/mnt/c/Users" ]]; then
+    # Copy pkg.m4 for Windows 10 bash mode use. If this isn't done,
+    # you'll PKGCONFIG lines won't be substituted in ./configure
+    cp /usr/share/aclocal/pkg.m4 m4
+fi
+
