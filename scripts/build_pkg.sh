f098482b4717e18ac0345eab5478c9762f556c21
Frank Hunleth <fhunleth@troodon-software.com>
2016-09-16 22:56:48 -0400

    Add support for building Raspbian packages

--- scripts/build_pkg.sh
+++ scripts/build_pkg.sh
@@ -97,8 +97,10 @@ if [[ "$CROSS_COMPILE" = "host" ]]; then
 
 fi
 
-# Run the regression tests
-make check
+if [[ "$CROSS_COMPILE" != "arm-linux-gnueabihf" ]]; then
+    # Run the regression tests
+    make check
+fi
 
 make install-strip
 make dist
@@ -133,12 +135,12 @@ if [[ "$SKIP_PACKAGE" != "true" ]]; then
         # Build Windows package
         rm -f fwup.exe
         cp $FWUP_INSTALL_DIR/bin/fwup.exe .
-        
+
         mkdir -p $FWUP_INSTALL_DIR/fwup/tools
         cp -f scripts/fwup.nuspec $FWUP_INSTALL_DIR/fwup/
         sed -i "s/%VERSION%/$(cat VERSION)/" $FWUP_INSTALL_DIR/fwup/fwup.nuspec
         cp $FWUP_INSTALL_DIR/bin/fwup.exe $FWUP_INSTALL_DIR/fwup/tools/
-        
+
         pushd $FWUP_INSTALL_DIR/fwup/
         rm -f *.nupkg
         export ChocolateyInstall=$DEPS_INSTALL_DIR/chocolatey
@@ -146,6 +148,26 @@ if [[ "$SKIP_PACKAGE" != "true" ]]; then
         popd
         rm -f *.nupkg
         cp $FWUP_INSTALL_DIR/fwup/*.nupkg .
+    elif [[ "$CROSS_COMPILE" = "arm-linux-gnueabihf" ]]; then
+        # Build Raspberry Pi package
+
+        # Fix directory permissions for packaging
+        find $FWUP_STAGING_DIR -type d | xargs chmod 755
+
+        # Debian requires compressed man pages
+        # NOTE: Even though building man pages is optional, it is
+        #       an error if man pages don't exist when creating packages
+        gzip -9 -f $FWUP_STAGING_DIR/usr/share/man/man1/fwup.1
+
+        # Build Raspbian package
+        rm -f fwup_*.deb
+        fpm -s dir -t deb -v $FWUP_VERSION -n fwup -m "$FWUP_MAINTAINER" \
+            --license "$FWUP_LICENSE" --description "$FWUP_DESCRIPTION" \
+            --depends "libc6 (>= 2.4)" --deb-priority optional --category devel \
+            -a armhf \
+            --url "$FWUP_HOMEPAGE" --vendor "$FWUP_VENDOR" -C $FWUP_STAGING_DIR
+
+
     fi
 else
     echo "Static build was successful, but skipped creating packages."
