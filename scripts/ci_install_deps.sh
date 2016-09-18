800a19fe306bf8a2fa5a16be602bc87db98b66a5
Frank Hunleth <fhunleth@troodon-software.com>
2016-09-17 21:37:16 -0400

    Use -qq on apt-get install calls

--- scripts/ci_install_deps.sh
+++ scripts/ci_install_deps.sh
@@ -52,13 +52,13 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
         windows)
             sudo dpkg --add-architecture i386
             sudo apt-get update
-            sudo apt-get install -y gcc-mingw-w64-x86-64 wine
+            sudo apt-get install -qq gcc-mingw-w64-x86-64 wine
 
             # These are needed for building choco
             sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
             echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/3.12.0 main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
             sudo apt-get update
-            sudo apt-get install mono-devel mono-gmcs nuget
+            sudo apt-get install -qq mono-devel mono-gmcs nuget
             ;;
         dynamic)
             sudo apt-get install -qq libarchive-dev
