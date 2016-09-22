07ba22ec859528f57199acd89e3c5a1cbd64325f
Michael K. Schmidt <michael.k.schmidt@schneider-electric.com>
2016-09-22 17:10:14 +0000

    Windows: Keep Logical Volumes locked while writing

--- src/mmc_windows.c
+++ src/mmc_windows.c
@@ -166,10 +166,12 @@ static void unmount_volume(LPWSTR volume_name) {
                              &bytes_returned,
                              NULL);
 
-    CloseHandle(volume_handle);
 
     if (!status)
-        fwup_errx(EXIT_FAILURE, "Error unmounting '%S' (Error %lu)", volume_name, GetLastError());
+        fwup_errx(EXIT_FAILURE, "Error unmounting '%S' (Error %lu)", volume_name, GetLastError());   
+
+    // Note we deliberately do not FSCTL_UNLOCK_VOLUME or call CloseHandle, as the Logical Volume must
+    // remained locked.  We rely on Windows to cleanup for us
 }
 
 /*
