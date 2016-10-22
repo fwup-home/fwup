7d23b5206fcc1c7e51b1e235a5fb28ffa6d68251
Frank Hunleth <fhunleth@troodon-software.com>
2016-10-21 22:33:58 -0400

    Fix spelling error

--- src/fwup_genkeys.c
+++ src/fwup_genkeys.c
@@ -49,9 +49,9 @@ int fwup_genkeys()
     OK_OR_CLEANUP(save_key("fwup-key.pub", pk, sizeof(pk)));
     OK_OR_CLEANUP(save_key("fwup-key.priv", sk, sizeof(sk)));
 
-    printf("Firmware signing keys created and saved to fwup-key.pub and fwup.priv\n\n");
+    printf("Firmware signing keys created and saved to fwup-key.pub and fwup-key.priv\n\n");
     printf("Distribute fwup-key.pub with your system so that firmware updates can be\n");
-    printf("authenticated. Keep fwup.priv in a safe location.\n");
+    printf("authenticated. Keep fwup-key.priv in a safe location.\n");
 
     return 0;
 
