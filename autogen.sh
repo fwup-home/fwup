e96eac97afb005bd8d18763ec874f446da737e7d
Frank Hunleth <fhunleth@troodon-software.com>
2016-11-30 05:15:37 -0500

    Remove bash-isms from autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 autoreconf --install
 
-if [[ -d "/mnt/c/Users" ]]; then
+if [ -d "/mnt/c/Users" ]; then
     # Copy pkg.m4 for Windows 10 bash mode use. If this isn't done,
     # you'll PKGCONFIG lines won't be substituted in ./configure
     cp /usr/share/aclocal/pkg.m4 m4
