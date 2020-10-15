60530693b277d026ad096c0cd09dd33d36d4dff9
Frank Hunleth <fhunleth@troodon-software.com>
2020-10-15 10:39:06 -0400

    Update mono installation for Ubuntu 20.04

--- scripts/ubuntu_install_chocolatey.sh
+++ scripts/ubuntu_install_chocolatey.sh
@@ -13,10 +13,10 @@ source $BASE_DIR/scripts/common.sh
 
 # Install mono if necessary
 if [ ! -f /usr/bin/nuget ]; then
-   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-   echo "deb http://download.mono-project.com/repo/debian wheezy/snapshots/3.12.0 main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-   sudo apt-get update
-   sudo apt-get install -qq mono-devel mono-gmcs nuget
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+    sudo apt update
+    sudo apt install -qq mono-devel nuget
 fi
 
 # Ensure that some directories exist
