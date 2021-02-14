ad46026b4aabdc7aea0a2413fd1d9648a26da788
Rick Carlino <rick.carlino@gmail.com>
2021-02-14 09:22:06 -0600

    Add --yes to apt install mono...

--- scripts/ubuntu_install_chocolatey.sh
+++ scripts/ubuntu_install_chocolatey.sh
@@ -16,7 +16,7 @@ if [ ! -f /usr/bin/nuget ]; then
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
     echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | tee /etc/apt/sources.list.d/mono-official-stable.list
     apt update
-    apt install -qq mono-devel nuget
+    apt install -qq mono-devel nuget --yes
 fi
 
 # Ensure that some directories exist
