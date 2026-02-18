428350a99f15c80bd4f822e51de828d346d8d068
Frank Hunleth <fhunleth@troodon-software.com>
2026-02-18 08:48:12 -0500

    Fix libconfuse download URL

--- scripts/download_deps.sh
+++ scripts/download_deps.sh
@@ -28,7 +28,7 @@ echo "Downloading third party libraries to $DOWNLOAD_DIR..."
 
 cd $DOWNLOAD_DIR
 [ -e zlib-$ZLIB_VERSION.tar.gz ] || curl -LO https://zlib.net/fossils/zlib-$ZLIB_VERSION.tar.gz
-[ -e confuse-$CONFUSE_VERSION.tar.gz ] || curl -LO https://github.com/martinh/libconfuse/releases/download/v$CONFUSE_VERSION/confuse-$CONFUSE_VERSION.tar.gz
+[ -e confuse-$CONFUSE_VERSION.tar.gz ] || curl -LO https://github.com/libconfuse/libconfuse/releases/download/v$CONFUSE_VERSION/confuse-$CONFUSE_VERSION.tar.gz
 [ -e libarchive-$LIBARCHIVE_VERSION.tar.gz ] || curl -LO https://libarchive.org/downloads/libarchive-$LIBARCHIVE_VERSION.tar.gz
 
 echo "Verifying checksums..."
